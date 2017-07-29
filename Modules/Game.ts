da8d3fe7534cd6a8640faeee350a52e0ff58f616
Tom A. Vibeto <tom@onlyhuman.dk>
2017-07-29 11:17:47 +0200

    Added links for later

--- Modules/Game.ts
+++ Modules/Game.ts
@@ -1,5 +1,9 @@
 import { terminal } from "terminal-kit";
 
+// https://www.npmjs.com/package/terminal-kit
+// https://github.com/cronvel/terminal-kit/blob/77af84556581c3e9f2410fa006f8afe62dbd0536/doc/global-api.md#top
+// http://blog.soulserv.net/tag/terminal/
+
 export class Engine {
 	constructor() {
 		console.log('test 2');
